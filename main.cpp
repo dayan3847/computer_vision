ea85d331350216e10658ef40bb1acf30895a0e03
Dayan Bravo <dayan3847@gmail.com>
2023-03-01 01:02:03 -0600

    refactor: simple example

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
+using namespace cv;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
+
+    Mat A = Mat::zeros(4, 4, CV_8UC1);
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            A.at<unsigned char>(i, j) = 128 + i * 10 + j * 5;
+
+    cout << "A = " << A << endl;
     return 0;
 }
