039c502812b9ac845c829abc8c6438746b5232f4
Dayan Bravo <dayan3847@gmail.com>
2023-03-16 12:19:54 -0600

    feat: read image example

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
+using namespace cv;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
+
+    // Image
+    string name = "img/jaguarcito.png";
+    Mat image = imread(name, IMREAD_GRAYSCALE);
+    cout << "Image: \"" << name << "\" (" << image.rows << "x" << image.cols << ")" << endl;
+
+    imshow(name, image);
+    waitKey(0);
+    destroyWindow(name);
+
     return 0;
 }
