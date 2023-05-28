b71f3772c6d9fce1ec7f3be16752693a6e81e0b2
Dayan Bravo <dayan3847@gmail.com>
2023-05-28 13:04:30 -0600

    check matrices

--- plane_tracker/tests/test_print_matriz.cpp
+++ plane_tracker/tests/test_print_matriz.cpp
@@ -5,17 +5,22 @@ int main()
 {
 	cv::Mat K = my_config::K();
 	my_tools::printMat(K, "K");
+	my_tools::saveMatInTxt(K, "K");
 
 	// Inverse of K
 	cv::Mat iiK;
 	cv::invert(K, iiK);
 	my_tools::printMat(iiK, "iiK");
+	my_tools::saveMatInTxt(iiK, "iiK");
 
 	cv::Mat iK = my_config::iK();
 	my_tools::printMat(iK, "iK");
+	my_tools::saveMatInTxt(iK, "iK");
+
 
 	cv::Mat I0 = my_config::I0();
 	my_tools::printMat(I0, "I0");
+	my_tools::saveMatInTxt(I0, "I0");
 
 	return 0;
 }
